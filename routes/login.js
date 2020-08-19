6717b3ae67c08499f240412ab247af1a27c147c4
Istom1n <iv@istm.ml>
2020-08-19 13:58:48 +0300

    Update texts

--- routes/login.js
+++ routes/login.js
@@ -2,34 +2,33 @@ var express = require('express');
 var router = express.Router();
 
 module.exports = function (passport, authenticationMiddleware) {
-
     /* GET login page. */
-    router.get('/', function(req, res, next) {
+    router.get('/', function (req, res, next) {
         res.render('login', {
-            brand: "Event+",
+            brand: 'Event+',
             show_brand: false,
             title: 'Login',
             left_menu: [
                 {
-                    href: "/",
-                    text: "Main",
-                    active: true
+                    href: '/',
+                    text: 'Main',
+                    active: true,
                 },
                 {
-                    href: "/wiki",
-                    text: "WiKi",
-                    active: false
-                }
+                    href: '/wiki',
+                    text: 'Wiki',
+                    active: false,
+                },
             ],
             right_menu: [
                 {
-                    href: "/signup",
-                    text: "Signup",
-                    active: false
-                }
-            ]
+                    href: '/signup',
+                    text: 'Sign Up',
+                    active: false,
+                },
+            ],
         });
     });
 
     return router;
-}
+};
--- routes/signup.js
+++ routes/signup.js
@@ -2,35 +2,33 @@ var express = require('express');
 var router = express.Router();
 
 module.exports = function (passport, authenticationMiddleware) {
-
     /* GET register page. */
-    router.get('/', function(req, res, next) {
+    router.get('/', function (req, res, next) {
         res.render('signup', {
-            brand: "Event+",
+            brand: 'Event+',
             show_brand: false,
             title: 'Signup',
             left_menu: [
                 {
-                    href: "/",
-                    text: "Main",
-                    active: true
+                    href: '/',
+                    text: 'Main',
+                    active: true,
                 },
                 {
-                    href: "/wiki",
-                    text: "WiKi",
-                    active: false
-                }
+                    href: '/wiki',
+                    text: 'Wiki',
+                    active: false,
+                },
             ],
             right_menu: [
                 {
-                    href: "/login",
-                    text: "Login",
-                    active: false
-                }
-            ]
+                    href: '/login',
+                    text: 'Login',
+                    active: false,
+                },
+            ],
         });
     });
 
     return router;
-
-}
+};
